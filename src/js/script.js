4c0cc79c8481f0e6b47d54320862c4e6e813a34f
Leonardo Silva <109092809+LeonardoS1lva@users.noreply.github.com>
2024-04-03 12:50:35 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+const botaoTrailer = document.getElementById("button-trailer");
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+function alternarModal() {
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
